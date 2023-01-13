40eba39537996e3cfc5521fee242881a550141e2
mykolasbar <106963436+mykolasbar@users.noreply.github.com>
2023-01-13 16:03:41 +0200

    ixing storage issues

--- app/Http/Controllers/schoolsController.php
+++ app/Http/Controllers/schoolsController.php
@@ -92,7 +92,7 @@ class schoolsController extends Controller
         if ($request->hasFile('picture')) {
             $path = $request->picture->storeAs('images', $request->picture->getClientOriginalName(), 's3');
             // $path = $request->picture->storeAs('images', $request->picture->getClientOriginalName());
-            Storage::disk('s3')->setVisibility($path, 'public');
+            // Storage::disk('s3')->setVisibility($path, 'public');
         }
 
         $newSchool = new Schools();
