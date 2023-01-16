e286649e1f26afe545300f95707b287f0570109b
mykolasbar <106963436+mykolasbar@users.noreply.github.com>
2023-01-16 22:15:32 +0200

    Storage

--- app/Http/Controllers/schoolsController.php
+++ app/Http/Controllers/schoolsController.php
@@ -90,10 +90,10 @@ class schoolsController extends Controller
         ]);
 
         if ($request->hasFile('picture')) {
-            // $path = $request->picture->storeAs('images', $request->picture->getClientOriginalName(), 's3');
+            $path = $request->picture->storeAs('images', $request->picture->getClientOriginalName(), 's3');
             // $path = $request->picture->storeAs('images', $request->picture->getClientOriginalName());
             // Storage::disk('s3')->setVisibility($path, 'public');
-            $path = $request->file('picture')->storePubliclyAs('images', $request->picture->getClientOriginalName(), 's3');
+            // $path = $request->file('picture')->storePubliclyAs('images', $request->picture->getClientOriginalName(), 's3');
         }
 
         $newSchool = new Schools();
